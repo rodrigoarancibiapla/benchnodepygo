7f92a812021db292a2723087873d2b15e56862c0
Rodrigo Arancibia <rodrigoimasd@gmail.com>
2024-08-05 11:43:23 -0400

    go update

--- go/app.go
+++ go/app.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 	"runtime"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -40,20 +41,93 @@ func generatePerson() map[string]interface{} {
 	return map[string]interface{}{"email": email, "age": age}
 }
 
+func generatePeople(numPeople int, numWorkers int) []map[string]interface{} {
+	people := make([]map[string]interface{}, numPeople)
+	var wg sync.WaitGroup
+	var rwMutex sync.RWMutex
+
+	// Dividir el trabajo entre los trabajadores
+	chunkSize := (numPeople + numWorkers - 1) / numWorkers
+
+	for w := 0; w < numWorkers; w++ {
+		wg.Add(1)
+		go func(workerId int) {
+			defer wg.Done()
+			start := workerId * chunkSize
+			end := start + chunkSize
+			if end > numPeople {
+				end = numPeople
+			}
+
+			for i := start; i < end; i++ {
+				person := generatePerson()
+				rwMutex.Lock()
+				people[i] = person
+				rwMutex.Unlock()
+			}
+		}(w)
+	}
+
+	wg.Wait()
+	return people
+}
+
+func generatePeople_0(numPeople int, numWorkers int) []map[string]interface{} {
+	people := make([]map[string]interface{}, numPeople)
+	jobs := make(chan int, numPeople)
+	results := make(chan map[string]interface{}, numPeople)
+
+	var wg sync.WaitGroup
+
+	// Crear trabajadores
+	for w := 1; w <= numWorkers; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for range jobs {
+				results <- generatePerson()
+			}
+		}()
+	}
+
+	// Enviar trabajos a los trabajadores
+	go func() {
+		for i := 0; i < numPeople; i++ {
+			jobs <- i
+		}
+		close(jobs)
+	}()
+
+	// Recoger los resultados
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// Almacenar los resultados en el slice de people
+	i := 0
+	for result := range results {
+		people[i] = result
+		i++
+	}
+
+	return people
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	numCPU := runtime.NumCPU()
 
 	// Imprimir el número de CPUs
-	fmt.Printf("Número de CPUs disponibles: %d\n", numCPU)
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.GET("/people", func(c *gin.Context) {
-		people := make([]map[string]interface{}, 5000)
-		for i := 0; i < 5000; i++ {
-			people[i] = generatePerson()
-		}
+		rand.Seed(time.Now().UnixNano())
+
+		numPeople := 45000
+		numWorkers := 4 //runtime.NumCPU() // Número de núcleos disponibles
+		fmt.Println(numWorkers, " workers")
+		people := generatePeople(numPeople, numWorkers)
 
 		// Format JSON with indentation
 		peopleJSON, err := json.MarshalIndent(people[:20], "", "    ")
